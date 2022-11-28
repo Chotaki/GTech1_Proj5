5163b8dfcb08c905a3c74c8d5e320e537d81eb20
Bcley <bruno.cleymand@gmail.com>
2022-11-28 11:29:39 +0100

    update

--- components/nav.php
+++ components/nav.php
@@ -2,10 +2,16 @@
     <div class="nav-wrapper">
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul class="hide-on-med-and-down">
-            <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
-            <li><a href="japanexpo.html">Japan Expo</a></li>
-            <li><a href="charterkg.html">Graphic Charter</a></li>
-            <li><a href="ijso.html">IJSO 2019</a></li>
+            <div>
+                <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
+                <li><a href="japanexpo.html">Japan Expo</a></li>
+                <li><a href="charterkg.html">Graphic Charter</a></li>
+                <li><a href="ijso.html">IJSO 2019</a></li>
+            </div>
+            <div class="right">
+                <li><a href="">Sign In</a></li>
+                <li><a href="">Sign Up</a></li>
+            </div>
         </ul>
     </div>
 </nav>
