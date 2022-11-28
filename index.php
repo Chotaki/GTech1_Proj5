d8b2d58e569724c7e53b071e1da12936d3e51142
cho <aromanzin@gaming.tech>
2022-11-28 10:40:08 +0100

    update

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen">
+    <!-- My css -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <!-- Animate -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+    <meta charset="UTF-8">
+    <meta name="description" content="project presentation">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Prompt:wght@800&display=swap" rel="stylesheet">
+
+    <!-- Browser tab -->
+    <link rel="icon" href="img/puzzle.png">
+    <title>Projects</title>
+  </head>
+
+  <body>
+
+    <!-- NavBar -->
+    <nav id="navbar" class="sticky red lighten-2">
+      <div class="nav-wrapper">
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="hide-on-med-and-down">
+          <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
+          <li><a href="japanexpo.php">Japan Expo</a></li>
+          <li><a href="charterkg.php">Graphic Charter</a></li>
+          <li><a href="ijso.php">IJSO 2019</a></li>
+          <li><a href="#modal1">Sign In</a></li>
+          <li><a href="#modal2">Sign Up</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div id="modal1" class="modal">
+              <div class="modal-content red lighten-2">
+                <h4 class="black-text center-align white-text">Sign in</h4>
+                <div class="container">
+                  <form class="row">
+                    <div class="input-field col s12 m6 offset-m3">
+                      <i class="material-icons prefix white-text">contact_mail</i>
+                      <input id="email" type="email" class="validate">
+                      <label for="email" class="white-text">Username</label>
+                      <span class="helper-text" data-error="Invalid Adress" data-success=""></span>
+                    </div>
+                    <div class="row">
+                      <div class="input-field col s12 m6 offset-m3">
+                        <i class="material-icons prefix white-text">contact_mail</i>
+                        <input id="email" type="email" class="validate">
+                        <label for="email" class="white-text">Email</label>
+                        <span class="helper-text" data-error="Invalid Adress" data-success=""></span>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="input-field col s12 m6 offset-m3">
+                        <i class="material-icons prefix white-text">contact_mail</i>
+                        <input id="email" type="email" class="validate">
+                        <label for="email" class="white-text">Passwod</label>
+                        <span class="helper-text" data-error="Invalid Adress" data-success=""></span>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+
+    <div id="modal2" class="modal">
+    <div class="modal-content red lighten-2">
+      <h4 class="black-text center-align white-text">Any questions ?</h4>
+      <div class="container">
+        <form class="row">
+          <div class="input-field col s12 m6 offset-m3">
+            <i class="material-icons prefix white-text">contact_mail</i>
+            <input id="email" type="email" class="validate">
+            <label for="email" class="white-text">Email</label>
+            <span class="helper-text" data-error="Invalid Adress" data-success=""></span>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix white-text">mode_edit</i>
+              <textarea id="icon_prefix2" class="materialize-textarea white-text"></textarea>
+              <label for="icon_prefix2" class="white-text">Your message here</label>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    
+    <ul class="sidenav" id="mobile-demo">
+      <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
+      <li><a href="japanexpo.html">Japan Expo</a></li>
+      <li><a href="charterkg.html">Graphic Charter</a></li>
+      <li><a href="ijso.html">IJSO 2019</a></li>
+    </ul>
+
+    <div class="death">
+      <div class="header-bg">
+        <!-- Header -->
+        <h1 id="header" class="center-align margin-header white-text animate__animated animate__fadeIn wow">Projects</h1>
+
+        <!-- Japan Expo -->
+        <div class="row container margin valign-wrapper">
+          <div class="col s12 m6 l6">
+            <div class="card hoverable animate__animated animate__slideInLeft wow">
+              <div class="card-image waves-effect waves-block waves-light">
+                <a href="japanexpo.html"><img class="activator" src="img/stage.JPG" alt="japan expo"></a>
+              </div>
+              <div class="card-content">
+                <span class="card-title grey-text text-darken-4">Japan Expo</span>
+              </div>
+            </div>  
+          </div>
+          <div class="col s12 m6 l6">
+            <div class="row">
+              <div class="col s12 m8 l8 offset-l2 offset-m2">
+                <div class="card-panel animate__animated animate__zoomIn wow">
+                  <span>Anaëlle participated at the Japan Expo 2022 with Kaio Gaming. She organized two tournaments, one of them being on Super Smash Bros and the other on Demon Slayer. She also had the chance to go and animate a Demon Slayer king of the hill on the biggest stage of the event.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="parallax-container">
+        <div class="parallax width"><img src="img/paralax.png" alt="background"></div>
+      </div>
+
+      <!-- Graphic Charter -->
+      <div class="row container margin valign-wrapper">
+        <div class="col s12 m6 l6">
+          <div class="row">
+            <div class="col s12 m8 l8 offset-l2 offset-m2">
+              <div class="card-panel animate__animated animate__zoomIn wow">
+                <span>Alongside Yohan Glad, the president of Kaio Gaming, Anaëlle has reworked the graphic charter of the association they are part of. It was a long process to create the global visual but what was even longer was to create merchandise with it.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m6 l6">
+          <div class="card hoverable animate__animated animate__slideInRight wow">
+            <div class="card-image waves-effect waves-block waves-light">
+              <a href="charterkg.html"><img class="activator" src="img/charter.png" alt="charter"></a>
+            </div>
+            <div class="card-content">
+              <span class="card-title grey-text text-darken-4">Graphic Charter</span>
+              </div>
+          </div>
+        </div>        
+      </div>
+
+      <div class="parallax-container">
+        <div class="parallax width"><img src="img/paralax.png" alt="background"></div>
+      </div>
+
+      <!-- IJSO 2019 -->
+      <div class="row container margin valign-wrapper">
+        <div class="col s12 m6 l6">
+          <div class="card hoverable animate__animated animate__slideInLeft wow">
+            <div class="card-image waves-effect waves-block waves-light">
+              <a href="ijso.html"><img class="activator" src="img/ijso.JPG" alt="ijso"></a>
+            </div>
+            <div class="card-content">
+              <span class="card-title grey-text text-darken-4">IJSO 2019</span>
+            </div>
+          </div>
+        </div>        
+        <div class="col s12 m6 l6">
+          <div class="row">
+            <div class="col s12 m8 l8 offset-l2 offset-m2">
+              <div class="card-panel animate__animated animate__zoomIn wow">
+                <span>In 2019, Anaëlle had the chance to be selected to participate in an international science olympiad in order to represent France alongside 5 other students. The olympiad took place in Doha, Qatar and gathered altogether 6 students and 3 teachers from over 70 different countries.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="parallax-container">
+        <div class="parallax width"><img src="img/paralax.png" alt="background"></div>
+      </div>
+
+      <!-- Dev Team -->
+      <div class="row container margin">
+
+        <h2 class="center-align">
+          Website Dev Team
+        </h2>
+
+        <div class="col s12 m6 l6 center-align">
+          <div class="card animate__animated animate__backInUp wow">
+            <div class="row">
+              <div class="col s12 m12 l6 center-align">
+                <img class="activator round" src="img/anaelle.jpg" alt="anaelle">
+              </div>
+              <div class="col s12 m12 l6 card-content left-align">
+                <ul>
+                <li><span class="card-title grey-text text-darken-4">ROMANZIN Anaëlle</span></li>
+                <li><a class="waves-effect waves-light red lighten-2 btn" href="https://www.linkedin.com/in/ana%C3%ABlle-romanzin-a10569251/" target="blank" rel="nofollow">LinkedIn</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="center-align valign-wrapper card-content">
+              <p>Student at Gaming Campus, future Game Dev and member of Kaio Gaming to organize eSports tournaments, participating in the creation of gaming related events and community management</p>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m6 l6 center-align">
+          <div class="card animate__animated animate__backInUp wow">
+            <div class="row">
+              <div class="col s12 m12 l6 center-align">
+                <img class="activator round" src="img/bruno.jpg" alt="bruno">
+              </div>
+              <div class="col s12 m12 l6 card-content left-align">
+                <ul>
+                  <li><span class="card-title grey-text text-darken-4">CLEYMAND Bruno</span></li>
+                  <li><a class="waves-effect waves-light red lighten-2 btn" href="https://www.linkedin.com/in/bruno-cleymand-22aaa4253/" target="blank" rel="nofollow">LinkedIn</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="center-align valign-wrapper card-content">
+              <p>16 years old student in his first year in GTech at Gaming Campus, Lyon, yearning for new experience to finally be able to show off on "present your past projects" assignments, and maybe make videogames too.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Footer -->
+      <footer class="page-footer">
+        <div class="row center-align">
+          <div class="col s12 m4 l4 center-align">
+            <img id="gcicon" class="responsive-img" src="img/gamingcampus.png" alt="logo gaming campus">
+          </div>
+
+          <div class="white-text container col s12 m4 l4 not-img center-align">
+            <h3>Our Emails</h3>
+            <p>aromanzin@gaming.tech <br>
+            bcleymand@gaming.tech</p>
+          </div>
+          <div class="col s12 m4 l4 not-img center-align">
+            <!-- Modal Trigger -->
+            <a class="waves-effect waves-light btn-large modal-trigger pink lighten-1" href="#modal3">Contact us !<i class="material-icons right">mail_outline</i></a>
+            <!-- Modal Structure -->
+            <div id="modal3" class="modal">
+              <div class="modal-content red lighten-2">
+                <h4 class="black-text center-align white-text">Any questions ?</h4>
+                <div class="container">
+                  <form class="row">
+                    <div class="input-field col s12 m6 offset-m3">
+                      <i class="material-icons prefix white-text">contact_mail</i>
+                      <input id="email" type="email" class="validate">
+                      <label for="email" class="white-text">Email</label>
+                      <span class="helper-text" data-error="Invalid Adress" data-success=""></span>
+                    </div>
+                    <div class="row">
+                      <div class="input-field col s12">
+                        <i class="material-icons prefix white-text">mode_edit</i>
+                        <textarea id="icon_prefix2" class="materialize-textarea white-text"></textarea>
+                        <label for="icon_prefix2" class="white-text">Your message here</label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <a href="#!" class="modal-close waves-effect waves btn-flat">Cancel</a>
+                <button class="btn waves-effect waves-light pink lighten-1" type="submit" name="action">Send<i class="material-icons right">send</i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row footer-copyright">
+          <div class="col s12 m6 l6 center-align">
+           <p  class="grey-text text-lighten-4">&copy; Gaming Campus - 2022</p>
+          </div>
+          <div class="col s12 m6 l6 center-align">
+            <a  class="grey-text text-lighten-4" href="https://gamingcampus.fr/?utm_term=gaming%20campus&utm_campaign=Gaming+Campus+-+Marque&utm_source=adwords&utm_medium=ppc&hsa_acc=5656227748&hsa_cam=11532002367&hsa_grp=118137353731&hsa_ad=483696679366&hsa_src=g&hsa_tgt=kwd-423735683440&hsa_kw=gaming%20campus&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=Cj0KCQjw48OaBhDWARIsAMd966DPDHNvnESKud8um5xdaCbx1x1k3eEKmMFDSOfBztSD9FPHT5Do1W4aArgCEALw_wcB" target="_blank" rel="nofollow">Gaming Campus Website</a>
+          </div>
+          </div>
+      </footer>
+    </div>  
+
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/script.js"></script>
+
+  </body>
+</html>
