1cf87feb73e90d8105ac0251720b9bc0df894a13
Bcley <bruno.cleymand@gmail.com>
2022-11-28 11:00:29 +0100

    update

---
+++ components/nav.php
@@ -0,0 +1,18 @@
+<nav id="navbar" class="sticky red lighten-2">
+    <div class="nav-wrapper">
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="hide-on-med-and-down">
+            <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
+            <li><a href="japanexpo.html">Japan Expo</a></li>
+            <li><a href="charterkg.html">Graphic Charter</a></li>
+            <li><a href="ijso.html">IJSO 2019</a></li>
+        </ul>
+    </div>
+</nav>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a onClick="refreshPage()"><i class="material-icons">home</i></a></li>
+    <li><a href="japanexpo.html">Japan Expo</a></li>
+    <li><a href="charterkg.html">Graphic Charter</a></li>
+    <li><a href="ijso.html">IJSO 2019</a></li>
+</ul>
