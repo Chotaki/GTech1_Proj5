97b7cf1fd7cb085416611dcc3c2a7db869a074c9
Bcley <bruno.cleymand@gmail.com>
2022-11-29 11:10:06 +0100

    better login

--- queries/login.php
+++ queries/login.php
@@ -1,5 +1,5 @@
 <?php 
-require_once "config.php"; 
+require_once "../config/config.php"; 
 $sql = "SELECT * FROM user WHERE email='".$_POST['email']."' AND password='".$_POST['password']."'"; 
 $pre = $pdo->prepare($sql); 
 $pre->execute();
