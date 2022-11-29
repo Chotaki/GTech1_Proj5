eccd8b576f27fc3ba3f4f0100f45207270019832
cho <aromanzin@gaming.tech>
2022-11-29 09:39:14 +0100

    upadtes

---
+++ components/connexions.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="icon" href="img/puzzle.png">
+        <title>Connexions</title>
+    </head>
+    <body>
+        
+        <div>
+            <h1>Connexions</h1>
+
+            <h2>Signing In</h2>
+                <form method="post" action="login.php">
+                    <input type='email' name='email' />
+                    <input type='password' name='password' />
+                    <input type='submit' value='Me connecter' />
+                </form>
+
+            <h2>Signing Up</h2>
+                <form method="post" action="login.php">
+                    <input type='email' name='email' />
+                    <input type='password' name='password' />
+                    <input type='submit' value='Me connecter' />
+                </form>
+        </div>
+
+    </body>
+</html>
--- components/nav.php
+++ components/nav.php
@@ -1,3 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!--Import Google Icon Font-->
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen">
+    <!-- My css -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <!-- Animate -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+    <meta charset="UTF-8">
+    <meta name="description" content="project presentation">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Prompt:wght@800&display=swap" rel="stylesheet">
+</head>
+<body>
+ 
 <nav id="navbar" class="sticky red lighten-2">
     <div class="nav-wrapper">
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
@@ -9,8 +40,8 @@
                 <li><a href="ijso.html">IJSO 2019</a></li>
             </div>
             <div class="right">
-                <li><a href="">Sign In</a></li>
-                <li><a href="">Sign Up</a></li>
+                <li><a href="#modal1">Sign Up</a></li>
+                <li><a href="#modal2">Sign In</a></li>
             </div>
         </ul>
     </div>
@@ -21,4 +52,9 @@
     <li><a href="japanexpo.html">Japan Expo</a></li>
     <li><a href="charterkg.html">Graphic Charter</a></li>
     <li><a href="ijso.html">IJSO 2019</a></li>
+    <li><a href="#modal1">Sign Up</a></li>
+    <li><a href="#modal2">Sign In</i></a></li>
 </ul>
+
+</body>
+</html>
