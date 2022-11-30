c41bb81b063a79c5b548fd64b40aa3672911e32b
Bcley <bruno.cleymand@gmail.com>
2022-11-30 10:41:07 +0100

    yhauh

--- queries/login.php
+++ queries/login.php
@@ -1,12 +1,12 @@
 <?php 
-require_once "php/config/config.php"; 
+require_once "../config/config.php"; 
 $sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'"; 
 $pre = $pdo->prepare($sql); 
 $pre->execute();
 $user = $pre->fetch(PDO::FETCH_ASSOC);
 if(empty($user)){ //vérifie si le resultat est vide !
      //non connecté
-     echo "Utilisateur ou mot de passe incorrect !";
+     $_SESSION['error']=""
 }else{
      $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
      require_once "adminCheck.php"
