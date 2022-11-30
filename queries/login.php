892426f4ab204f9d3d41db7c9a80fc2d38b5dbea
Bcley <bruno.cleymand@gmail.com>
2022-11-30 11:29:04 +0100

    login hooh

--- queries/login.php
+++ queries/login.php
@@ -1,12 +1,17 @@
 <?php 
 require_once "../config/config.php"; 
-$sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'"; 
-$pre = $pdo->prepare($sql); 
-$pre->execute();
-$user = $pre->fetch(PDO::FETCH_ASSOC);
-$_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
-require_once "adminCheck.php"
-
+if (empty($_POST["username"])){
+    $_SESSION["error"]="Invalid Username";
+}elseif (empty($_POST["password"])){
+    $_SESSION["error"]="Invalid Password";
+}else{
+    $sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'"; 
+    $pre = $pdo->prepare($sql); 
+    $pre->execute();
+    $user = $pre->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+    require_once "adminCheck.php"
+};
 
 header('Location:index.php');//on le redirige sur la page d'accueil du site !
 exit();
