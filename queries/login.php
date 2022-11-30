ba92c23499f2f3874212f926dfd87bbc54efc3fd
Bcley <bruno.cleymand@gmail.com>
2022-11-30 11:39:16 +0100

    login lvl up

--- queries/login.php
+++ queries/login.php
@@ -1,16 +1,17 @@
-#login.php
 <?php 
 require_once "../config/config.php"; 
-$sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'"; 
-$pre = $pdo->prepare($sql); 
-$pre->execute();
-$user = $pre->fetch(PDO::FETCH_ASSOC);
-if(empty($user)){ //vérifie si le resultat est vide !
-     //non connecté
-     echo "Utilisateur ou mot de passe incorrect !";
+if (empty($_POST["username"])){
+    $_SESSION["error"]="Invalid Username";
+}elseif (empty($_POST["password"])){
+    $_SESSION["error"]="Invalid Password";
 }else{
-     $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
-}
+    $sql = "SELECT * FROM user WHERE username='".$_POST['username']."' AND password='".$_POST['password']."'"; 
+    $pre = $pdo->prepare($sql); 
+    $pre->execute();
+    $user = $pre->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+    require_once "adminCheck.php";
+};
 
 header('Location:../index.php');//on le redirige sur la page d'accueil du site !
 ?>
