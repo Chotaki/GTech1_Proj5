23246dfcc84ef1d39dd81cf98e4f633da9b2e94d
cho <aromanzin@gaming.tech>
2022-12-01 18:22:03 +0100

    u

--- queries/adminCheck.php
+++ queries/adminCheck.php
@@ -1,6 +1,18 @@
 <?php
+require_once("config/config.php");
+
 if (!isset($_SESSION['user']) || $_SESSION['user']["admin"]==0){
-    header('Location: index.php');
+    if(is_file("index.php")){
+        header('Location: index.php');
+    }else{
+        header('Location: ../index.php');
+    }
     exit();
+}else{
+    if(is_file("panelAdmin.php")){
+        header('Location: panelAdmin.php');
+    }else{
+        header('Location: ../panelAdmin.php');
+    }
 }
 ?>
